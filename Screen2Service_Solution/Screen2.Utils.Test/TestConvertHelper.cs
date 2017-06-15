cafa91e59b0701749d1fe70ba75dd4a7c0c6bd6f
Steven Jiang <stevenjiangnz@gmail.com>
2017-06-15 22:22:17 +1000

    add utils test project

---
+++ Screen2Service_Solution/Screen2.Utils.Test/TestConvertHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Screen2.Utils.Test
+{
+    [TestClass]
+    public class TestConvertHelper
+    {
+        [TestMethod]
+        public void Test_DateToInt()
+        {
+            DateTime date = DateTime.Now;
+
+            var dateInt = DateHelper.DateToInt(date);
+
+            Assert.IsTrue(dateInt == 20160301);
+        }
+    }
+}
---
+++ Screen2Service_Solution/Screen2.Utils.Test/TestDateHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screen2.Utils.Test
+{
+    [TestClass]
+    public class TestDateHelper
+    {
+        [TestMethod]
+        public void Test_DateToInt()
+        {
+            int dateInt = DateHelper.DateToInt(DateTime.Now);
+
+        }
+
+        [TestMethod]
+        public void Test_IntToDate()
+        {
+            int dateInt = DateHelper.DateToInt(DateTime.Now);
+            DateTime dt = DateHelper.IntToDate(dateInt);
+        }
+
+        [TestMethod]
+        public void Test_NextTradingDay()
+        {
+            int dateInt = DateHelper.DateToInt(DateTime.Now.AddDays(-2));
+            var nextDay = DateHelper.NextTradingDay(dateInt);
+        }
+
+        [TestMethod]
+        public void Test_IntToJSTicks()
+        {
+            DateTime dt= new DateTime(2001, 10, 10);
+
+            var tk = DateHelper.IntToJSTicks(20011010);
+
+            Assert.IsTrue(tk == 1002672000000);
+        }
+    }
+}
