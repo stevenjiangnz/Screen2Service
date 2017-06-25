cf1741840fd4a9518a48a0fbc89b128543cf49c7
Steven Jiang <stevenjiangnz@gmail.com>
2017-06-25 13:43:49 +1000

    add online check api

---
+++ Screen2Service_Solution/Screen2.API/Controllers/OnlineController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Screen2.DAL.Interface;
+
+namespace Screen2.Api.Controllers
+{
+    [RoutePrefix("api/online")]
+    public class OnlineController : BaseApiController
+    {
+        #region Constructor
+        public OnlineController(IUnitWork unit)
+            : base(unit)
+        {
+        }
+        #endregion
+
+        #region methods
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok("OK. Service is online.");
+        }
+        #endregion
+    }
+}
