3d4a098fa0f47ce3005fe19d1bd1c769bd66bff4
Steven Jiang <stevenjiangnz@gmail.com>
2017-06-27 22:26:11 +1000

    add test projects

---
+++ Screen2Service_Solution/Screen2.BLL.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Screen2.BLL.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true, "Test case passed");
+        }
+    }
+}
